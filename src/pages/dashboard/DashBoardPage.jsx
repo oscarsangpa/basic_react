603c6f403f13a0b06a67930a0fed6f52206150b9
Oscar Sangrador <oscarsangrador@gmail.com>
2022-08-26 13:27:00 +0200

    fix: migration Redirect to Navigate

---
+++ src/pages/dashboard/DashBoardPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+
+const DashBoardPage = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default DashBoardPage;
